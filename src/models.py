de063f2d1261354aaff5c9e5ea999369cb3d7afd
Alexader Quejada cordoba <quejadacordobaalexander@gmail.com>
2024-12-05 12:47:43 +0000

    starwars data1

--- src/models.py
+++ src/models.py
@@ -7,26 +7,70 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+# Tabla Usuario
+class usuario(Base):
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    nombre = Column(String(250), nullable=False)
+    edad = Column(Integer)
+    correo = Column(String(250), unique=True, nullable=False)
+    favoritos = relationship("favorito", back_populates="usuario")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+# Tabla Personaje
+class personaje(Base):
+     __tablename__ = 'personaje'
+     id = Column(Integer, primary_key=True)
+     nombre = Column(String(250), nullable=False)
+     edad = Column(Integer)
+     raza = Column(String(250))
+     altura = Column(String(250))
+     color_piel = Column(String(250))
 
-    def to_dict(self):
+# # Tabla Vehículos
+class vihicles(Base):
+     __tablename__ = 'vihicles'
+     id = Column(Integer, primary_key=True)
+     nombre = Column(String(100), nullable=False)
+     tripulacion = Column(Integer)
+     capacidad_carga = Column(Integer)
+     fabricante = Column(String(100), nullable=False)
+
+ # Tabla Planetas
+class planetas(Base):
+     __tablename__ = 'planetas'
+     id = Column(Integer, primary_key=True)
+     nombre = Column(String(100), nullable=False)
+     clima = Column(String(50), nullable=True)
+     terreno = Column(String(50), nullable=False)
+
+ # Tabla Armas
+class armas(Base):
+     __tablename__ = 'armas'
+     id = Column(Integer, primary_key=True)
+     nombre = Column(String(80), nullable=False)
+     tamano = Column(String(70), nullable=False)
+     creada_porquien = Column(String(300), nullable=False)
+
+ # Tabla Favorito
+class favorito(Base):
+     __tablename__ = 'favorito'
+     id = Column(Integer, primary_key=True)
+     usuario_id = Column(Integer, ForeignKey('usuario.id'), nullable=False)
+     personaje_id = Column(Integer, ForeignKey('personaje.id'), nullable=True)
+     vehicles_id = Column(Integer, ForeignKey('vihicles.id'), nullable=True)
+     planeta_id = Column(Integer, ForeignKey('planetas.id'), nullable=True)
+     armas_id = Column(Integer, ForeignKey('armas.id'), nullable=True)
+
+     usuario = relationship("usuario", back_populates="favoritos")
+
+# Método to_dict
+     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
